baeb736411a6095d7a3702f9430d08579b84a490
Wesley Webb <wwebb@brij.africa>
2024-08-02 19:52:29 +0000

    created the icon component

---
+++ src/components/icon.vue
@@ -0,0 +1,54 @@
+<script setup>
+import iconset from '@/assets/icons.json';
+import { computed } from '@vue/reactivity';
+
+defineOptions({name: 'icon-component'});
+const props = defineProps({
+  variant: {
+    type: String,
+    default: 'directbox-default.linear',
+  }
+});
+
+const paths = computed(() => {
+  const [{type, style}] = [...props.variant.matchAll(/^(?<type>[a-z\d-]+)\.(?<style>[a-z\d-]+)$/g)].map(e => e.groups ?? {});
+
+  console.log(type, style);
+
+  return iconset[type]?.[style] ?? [];
+});
+</script>
+
+<template>
+  <svg
+    class="icon-component"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path
+      class="icon-component__path"
+      :d="path.definition"
+      :key="index"
+      :style="{'--icon-component__path--opacity': path.opacity}"
+      v-for="(path, index) of paths"
+    ></path>
+  </svg>
+</template>
+
+<style>
+.icon-component {
+  display: inline-block;
+  block-size: 1.5em;
+  fill: none;
+  inline-size: 1.5em;
+  vertical-align: bottom;
+}
+
+.icon-component__path {
+  clip-rule: evenodd;
+  fill: currentColor;
+  fill-rule: evenodd;
+  opacity: var(--icon-component__path--opacity);
+}
+</style>
