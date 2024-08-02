fa5858bbc59bf8fc996e29ee8c8a9eeaeb48a352
Wesley Webb <wwebb@brij.africa>
2024-08-02 21:59:03 +0000

    fixed but with setting the api host

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import useAuthStore from '@/stores/auth.js';
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = import.meta.env.VITE_API_HOST;
 axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.headers.Accepted = 'application/json';
 
